94ad55895982a039416b36b2da9f4463bc8817cb
Gustavo Suto <guyrux@gmail.com>
2024-07-27 14:41:55 -0300

    chore: Changing plot_histogram's pivot parameter.

--- data_describe/plot.py
+++ data_describe/plot.py
@@ -10,13 +10,14 @@ from data_describe.utils import clean_data
 
 
 def describing_continuous_data(
-    df: pd.DataFrame, lst_columns: Union[list[str], str]
+    df: pd.DataFrame, lst_columns: Union[list[str], str], cleaning_data: bool = True
 ):
     """Describe a pd.DataFrame in each column within a given list.
 
     Args:
         df (pd.DataFrame):
         lst_columns (Union[list[str], str]):
+        clean_data (bool, optional): If True, the data will be cleaned before describing. Defaults to True.
 
     Returns:
         _type_: Return a pd.DataFrame with the describe of each column.
@@ -25,18 +26,20 @@ def describing_continuous_data(
     df_temp = pd.DataFrame()
 
     for column in lst_columns:
-        dct_temp[column] = clean_data(df=df, subset=[column], verbose=0)[
-            [column]
-        ]
+        if cleaning_data:
+            dct_temp[column] = clean_data(df=df, subset=[column], verbose=0)[
+                [column]
+            ]
+
         df_temp = pd.concat(
-            [df_temp, dct_temp[column].describe().T.reset_index()], axis=0
+            [df_temp, dct_temp[column].describe(percentiles=[5, 95]).T.reset_index()], axis=0
         )
 
     return df_temp
 
 
 def plot_histogram(
-    df: pd.DataFrame, column: str, pivot: float = np.nan
+    df: pd.DataFrame, column: str, pivot: Union[tuple, float] = np.nan
 ) -> None:
     """Plot a histogram with a kde and a vertical line for a given pivot value.
 
@@ -50,12 +53,19 @@ def plot_histogram(
     """
     plt.figure(figsize=(6, 2))
     sns.histplot(data=df, x=column, bins=50, kde=True)
-    plt.axvline(x=pivot, color='red', linestyle='--', linewidth=1)
-    text = f'Pivot: {pivot:.2f}'
+
+    if isinstance(pivot, dict):
+        pivot_str = f'{str(pivot[0])}: {pivot[1]:.2f}'
+        pivot_value = float(pivot[1])
+    else:
+        pivot_str = f'pivot: {pivot:.2f}'
+        pivot_value = float(pivot)
+
+    plt.axvline(x=pivot_value, color='red', linestyle='--', linewidth=1)
     plt.text(
-        pivot,
+        pivot_value,
         plt.gca().get_ylim()[1],
-        f'{text}',
+        f'{pivot_str}',
         horizontalalignment='right',
         verticalalignment='bottom',
         color='red',
